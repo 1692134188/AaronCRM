05ff68f63548691f3a63b31354c3d6d223b84b72
Aaron.Yang <1692134188@qq.com>
2019-11-01 10:24:11 +0800

    CRM 1.0 版本 框架+登录+

---
+++ KingAdmin/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from KingAdmin import views
+
+urlpatterns = [
+    url(r'^$', views.app_index, name="app_index"),
+    url(r'^login/', views.acc_login),
+    url(r'^logout/', views.acc_logout, name="logout"),
+]
---
+++ KingAdmin/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate,logout
+def app_index(request):
+    return render(request,'kingadmin/app_index.html')
+# Create your views here.
+def acc_login(request):
+    error_msg=""
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password= request.POST.get('password')
+
+        user=authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect(request.GET.get('next', '/kingadmin/'))
+        else:
+            error_msg = "Wrong username or password!"
+    return render(request,"kingadmin/login.html", {'error_msg':error_msg})
+
+def acc_logout(request):
+    logout(request)
+    return redirect("/kingadmin/login/")
